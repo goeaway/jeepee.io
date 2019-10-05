ff15d6acc97f542f2d41b589a84fef6273b7328e
Joe Thompson <j>
2019-10-05 23:01:12 +0100

    Add Controller web project

---
+++ Jeepee.IO.Controller.Presentation.Web/src/components/app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "../styles/site.less";
+
+const App = () => {
+    return (
+        <div>Hello</div>
+    )
+}
+
+export default App;
---
+++ Jeepee.IO.Controller.Presentation.Web/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./components/app";
+
+const div = document.getElementById("react-root");
+
+ReactDOM.render(
+    <App />,
+    div
+);
